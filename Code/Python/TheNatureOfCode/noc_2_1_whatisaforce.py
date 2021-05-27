662304d1c28ee8f068467c7b747c8a3c0b23a295
Yogesh Kulkarni <yogeshkulkarni@yahoo.com>
2021-05-27 12:08:08 +0530

    Added force tutorials

---
+++ Code/Python/TheNatureOfCode/noc_2_1_whatisaforce.py
@@ -0,0 +1,16 @@
+# The Nature of Code - Daniel Shiffman http://natureofcode.com
+# Example 2-1: What is a force?
+# PyP5 port by: Yogesh Kulkarni
+# No code
+# Reference Youtube Video: https://www.youtube.com/watch?v=rqecAdEGW6I&list=PLRqwX-V7Uu6aFlwukCmDf0-1-uSR7mklK&index=13
+
+from p5 import *
+
+def setup():
+    pass
+
+def draw():
+    pass
+
+if __name__ == "__main__":
+    run()
