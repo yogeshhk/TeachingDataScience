af7f68de2eca9b917fa70ef58b9baff41ea61ff5
Yogesh Kulkarni <yogeshkulkarni@yahoo.com>
2021-07-05 08:05:45 +0530

    Adding Patricks pytorch tutorial

---
+++ Code/PyTorch/PatrickLoeber/pytorch_01_installation.py
@@ -0,0 +1,7 @@
+# https://www.youtube.com/watch?v=EMXfZB8FVUA&list=PLqnslRFeH2UrcDBWF5mfPGpqQDSta6VK4&index=2
+
+import torch
+x = torch.rand(3)
+print(x)
+print(torch.__version__)
+print(torch.cuda.is_available())
