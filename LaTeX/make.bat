ad445e4d5e60e8ad50e36284e973646e7fb73943
Yogesh Kulkarni <yogeshkulkarni@yahoo.com>
2026-07-24 08:11:30 +0530

    Reorged python deck

--- LaTeX/make.bat
+++ LaTeX/make.bat
@@ -1,2 +1,3 @@
 @echo off
-for /r %%i in (Main_Course_ML_CoEP_*.tex) do texify -cp %%i
+for /r %%i in (Main_Seminar_Python_Overview*.tex) do texify -cp %%i
+REM for /r %%i in (Main_Course_ML_CoEP_*.tex) do texify -cp %%i
